a78e95edfaba9c31ff1d35d4575847486c136265
taras-sereda <taras.y.sereda@gmail.com>
2014-08-31 00:38:55 +0300

    itertools.combinations is already exists in python

--- tests/set_k_size_combinations_test.py
+++ tests/set_k_size_combinations_test.py
@@ -1,11 +1,17 @@
 __author__ = 'taras'
 
 from set_combinations import xuniqueCombinations
+import itertools
+
 
 #Test correctness
 assert list(xuniqueCombinations([1, 2, 3, 4], 2)) == [[1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4]]
 
 # Test amount of combinations
 assert len(list(xuniqueCombinations([1, 2, 3, 4, 5], 3))) == 10
+
+# Built-in functionality is already present!
+assert  (set(itertools.combinations({1, 2, 3, 4}, 2))) == {(1, 2), (1, 3), (1, 4), (2, 3), (3, 4), (2, 4)}
+
 
 
