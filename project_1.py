da301c30ad16736bd7dda86bdbec38a141f066ff
taras-sereda <taras.y.sereda@gmail.com>
2014-09-06 21:32:46 +0300

    project. Citation Graph analysis

--- project_1.py
+++ project_1.py
@@ -5,11 +5,33 @@ Provided code for Application portion of Module 1
 
 Imports physics citation graph
 """
+
 import urllib2
 import random
 import itertools
-import module_1
+from module_1 import in_degree_distribution
+from matplotlib import rcParams
 import matplotlib.pyplot as plt
+import numpy
+
+dark2_colors = [(0.10588235294117647, 0.6196078431372549, 0.4666666666666667),
+                (0.8509803921568627, 0.37254901960784315, 0.00784313725490196),
+                (0.4588235294117647, 0.4392156862745098, 0.7019607843137254),
+                (0.9058823529411765, 0.1607843137254902, 0.5411764705882353),
+                (0.4, 0.6509803921568628, 0.11764705882352941),
+                (0.9019607843137255, 0.6705882352941176, 0.00784313725490196),
+                (0.6509803921568628, 0.4627450980392157, 0.11372549019607843),
+                (0.4, 0.4, 0.4)]
+
+# size of gifures in inches horisontal,vertical
+rcParams['axes.color_cycle'] = dark2_colors
+# linewidth in points
+rcParams['lines.linewidth'] = 2
+#displays grid
+rcParams['axes.grid'] = True
+rcParams['axes.facecolor'] = '#eeeeee'
+rcParams['font.size'] = 14
+rcParams['patch.edgecolor'] = 'none'
 
 CITATION_URL = "http://storage.googleapis.com/codeskulptor-alg/alg_phys-cite.txt"
 
@@ -47,32 +69,37 @@ def compleate_directed_graph(num_nodes, prob):
         edges = set()
         for edge in nodes:
             if node != edge:
-                if random.random() > prob:
+                if random.random() < prob:
                     edges.add(edge)
         graph[node] = edges
 
-
     return graph
 
-if __name__ == '__main__':
-    print (compleate_directed_graph(12, 0.8))
+def normalize_distribution(distribution):
 
-# citation_graph = load_graph(CITATION_URL)
-#
-# un_normalized_distribution = module_1.in_degree_distribution(citation_graph)
-#
-# normalized_distribution = {key : val/27770.  for key, val in un_normalized_distribution.items()}
-#
-#
+    factor = 1.0/sum(distribution.values())
+    normalized_distribution = {key: val*factor for key, val in distribution.items()}
+
+    return normalized_distribution
+
+if __name__ == '__main__':
+
+    graph1 = normalize_distribution(in_degree_distribution(compleate_directed_graph(300, 0.4)))
+    graph2 = normalize_distribution(in_degree_distribution(compleate_directed_graph(300, 0.4)))
+    graph3 = normalize_distribution(in_degree_distribution(compleate_directed_graph(300, 0.4)))
+
+    plt.loglog(graph1.keys(), graph1.values(), 'go')
+    plt.show()
+
+    citation_graph = load_graph(CITATION_URL)
+    out_degree = {k: len(v) for (k,v) in citation_graph.items()}
+
+    print (numpy.mean(out_degree.values()))
+
+
 # x = itertools.islice(normalized_distribution.items(), 0, 10)
 # for key, val in x:
 #     print key, val
-#
-#
-# print (sum(normalized_distribution.values()))
-#
-# plt.loglog(normalized_distribution.keys(), normalized_distribution.values(), 'ro')
-# plt.show()
 
 
 
